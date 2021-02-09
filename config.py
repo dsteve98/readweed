b1229d922ecff906036949bbd4bccc54df6a5606
dsteve98 <dan.steve2910@gmail.com>
2021-02-09 16:44:35 +0700

    config.py change

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key = 'your consumer key'
-consumer_secret = 'your consumer secret'
-access_token = 'your access_token'
-access_secret = 'your access_secret'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
